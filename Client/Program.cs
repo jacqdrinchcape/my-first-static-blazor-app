558e0cd73d175b33e33c89ef532d640c286b3349
Jacqueline Dela Rosa <WebsiteHosting-DDC@inchcapeuk.onmicrosoft.com>
2021-12-15 08:01:24 +0800

    jadr-add authentication

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,4 +8,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorStaticWebApps.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://e3e6fee9-1173-4620-8c69-d20a17c96a36/BlazorHostedAPI.Access");
+});
+
 await builder.Build().RunAsync();
