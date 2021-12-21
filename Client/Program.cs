4e82dde215d413fe6ad9b233811c28e799c1a16a
Jacqueline Dela Rosa <WebsiteHosting-DDC@inchcapeuk.onmicrosoft.com>
2021-12-21 10:11:21 +0800

    JADR - Add configuration appsettins.json

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,18 +1,26 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var configuration = new ConfigurationBuilder()
+                                       .AddJsonStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("BlazorApp.Client.appsettings.json"))
+                                       .Build();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
 
+
 // Supply HttpClient instances that include access tokens when making requests to the server project
 //builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorStaticWebApps.ServerAPI"));
 
-
+builder.Services.AddTransient(_ => { return configuration.GetSection("GalaxyStuff").Get<GalaxyInfo>(); });
 
 builder.Services.AddMsalAuthentication(options =>
 {
