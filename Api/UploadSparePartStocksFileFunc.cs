f646a771214f77f6d1f9c7ed3c4293b4943e2bce
Jacqueline Dela Rosa <WebsiteHosting-DDC@inchcapeuk.onmicrosoft.com>
2022-01-05 11:50:55 +0800

    JADR - Add validation in fileupload

--- Api/UploadSparePartStocksFileFunc.cs
+++ Api/UploadSparePartStocksFileFunc.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Azure.Storage.Blobs;
+using Shared = BlazorApp.Shared;
 
 namespace BlazorApp.Api
 {
@@ -15,36 +16,82 @@ namespace BlazorApp.Api
     {
         [FunctionName("UploadSparePartStocksFileFunc")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "UploadSparePartStocksFileFunc")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "UploadSparePartStocksFileFunc")] HttpRequest req,
             ILogger log)
         {
+            
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             try
             {
-                var container = GetContainer();
-                string fileName = $"{Guid.NewGuid().ToString()}.csv";
-                var blob = container.GetBlobClient(fileName);
-                await blob.UploadAsync(req.Body);
+                int isValidDocument = await Shared.Helper.VerifyDocument(req.Body, "documenttypesparepartstock");
+                
+                if (isValidDocument == 0)
+                {
+                    var container = Shared.Helper.GetContainer("blobsparepartstockStorageContainer");
+                    string fileName = $"{Guid.NewGuid().ToString()}.csv";
+                    var blob = container.GetBlobClient(fileName);
+
+                    req.Body.Position = 0;
+                    await blob.UploadAsync(req.Body);
+                }
+                else
+                {
+                    return new BadRequestObjectResult(Shared.Helper.ErrorMessage(isValidDocument));
+                }
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                return new BadRequestObjectResult("Error saving file");
+                return new BadRequestObjectResult(e.Message.ToString());
             }
 
             return new OkObjectResult("ok");
         }
 
+     
         #region helper
-        private static BlobContainerClient GetContainer()
-        {
-            var blobConnectionString = Environment.GetEnvironmentVariable("blobConnectionString");
-            var blobsparepartstockStorageContainer = Environment.GetEnvironmentVariable("blobsparepartstockStorageContainer");
-            var container = new BlobContainerClient(blobConnectionString, blobsparepartstockStorageContainer);
-            container.CreateIfNotExists();
+        //private static BlobContainerClient GetContainer()
+        //{
+        //    var blobConnectionString = Environment.GetEnvironmentVariable("blobConnectionString");
+        //    var blobsparepartstockStorageContainer = Environment.GetEnvironmentVariable("blobsparepartstockStorageContainer");
+        //    var container = new BlobContainerClient(blobConnectionString, blobsparepartstockStorageContainer);
+        //    container.CreateIfNotExists();
 
-            return container;
-        }
+        //    return container;
+        //}
+
+        //private static int VerifyDocument(Stream body, string documenttypesparepartstock)
+        //{
+        //    int resp = 0;
+
+        //    var reader = new StreamReader(body);
+        //    var lineNumber = 1;
+        //    var line = reader.ReadLine();
+        //    while (lineNumber <= 2)
+        //    {
+        //        //Verify document_type - first column
+        //        if (lineNumber == 2)
+        //        {
+        //            line = reader.ReadLine();
+
+        //            //Empty file
+        //            if (line == null)
+        //            {
+        //                return 1;
+        //            }
+        //            var parts = line.Split(',');
+
+        //            if (parts[0] != Environment.GetEnvironmentVariable("documenttypesparepartstock"))
+        //            {
+        //               return 1;
+        //            }
+                    
+        //        }
+        //        lineNumber++;
+        //    }
+
+        //    return resp;
+        //}
         #endregion
     }
 }
---
+++ Shared/Helper.cs
@@ -0,0 +1,89 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class Helper
+    {
+        public static BlobContainerClient GetContainer(string blobContainer)
+        {
+            var blobConnectionString = Environment.GetEnvironmentVariable("blobConnectionString");
+            var blobStorageContainer = Environment.GetEnvironmentVariable(blobContainer);
+            var container = new BlobContainerClient(blobConnectionString, blobStorageContainer);
+            container.CreateIfNotExists();
+
+            return container;
+        }
+
+        public static async Task<int>  VerifyDocument(Stream body, string documenttype)
+        {
+            int resp = 0;
+            if (body.Length == 0)
+            {
+                //Empty File
+                return 1;
+            }
+
+            var reader = new StreamReader(body);
+            var lineNumber = 1;
+            var line = await reader.ReadLineAsync();
+            while (lineNumber <= 2)
+            {
+                //Verify document_type - first column
+                if (lineNumber == 2)
+                {
+                    line = await reader.ReadLineAsync();
+
+                    //Empty line
+                    if (line == null)
+                    {
+                        return 2;
+                    }
+                    var parts = line.Split(',');
+
+                    //Invalid Document Type
+                    if (parts[0] != Environment.GetEnvironmentVariable(documenttype))
+                    {
+                        return 3;
+                    }
+
+                }
+                lineNumber++;
+            }
+
+            return resp;
+        }
+
+
+
+        public static string ErrorMessage(int errorCode)
+        {
+            string errorMessage = string.Empty;
+            switch (errorCode)
+            {
+                case 1:
+                    errorMessage = "Empty File...";
+                    break;
+
+                case 2:
+                    errorMessage = "Empty Line...";
+                    break;
+
+                case 3:
+                    errorMessage = "Invalid Document Type...";
+                    break;
+
+                default: errorMessage = "Error";
+                    break;
+            }
+               
+            return errorMessage;
+
+        }
+
+    }
+}
